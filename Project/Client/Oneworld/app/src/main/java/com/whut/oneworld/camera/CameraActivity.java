f9d269fef495f43e1ddeb1d935fc805938c4b38f
1481660657@qq.com <tsrl5201314>
2020-01-07 09:58:25 +0800

    camera

--- Project/Client/Oneworld/app/src/main/java/com/whut/oneworld/camera/CameraActivity.java
+++ Project/Client/Oneworld/app/src/main/java/com/whut/oneworld/camera/CameraActivity.java
@@ -1,10 +1,14 @@
 package com.whut.oneworld.camera;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.config.PictureMimeType;
 import com.whut.oneworld.R;
+import com.whut.oneworld.util.GlideEngine;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -12,5 +16,25 @@ public class CameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        PictureSelector.create(CameraActivity.this)
+                .openGallery(PictureMimeType.ofImage())
+                .isUseCustomCamera(false)
+                .isOriginalImageControl(true)
+                .isWeChatStyle(true)
+                .isAndroidQTransform(false)
+                .enableCrop(false)
+                .maxSelectNum(1)
+                .imageSpanCount(4)
+                .isReturnEmpty(false)
+                .cameraFileName("Test.png")
+                .isCamera(true)
+                .loadImageEngine(GlideEngine.createGlideEngine())
+                .forResult(PictureConfig.CHOOSE_REQUEST);
+
+//        PictureSelector.create(this)
+//                .openCamera(PictureMimeType.ofImage())
+//                .loadImageEngine(GlideEngine.createGlideEngine())
+//                .forResult(PictureConfig.REQUEST_CAMERA);
     }
 }
--- Project/Client/Oneworld/app/src/main/java/com/whut/oneworld/search/SearchActivity.java
+++ Project/Client/Oneworld/app/src/main/java/com/whut/oneworld/search/SearchActivity.java
@@ -6,11 +6,22 @@ import android.os.Bundle;
 
 import com.whut.oneworld.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import co.lujun.androidtagview.TagContainerLayout;
+
 public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        TagContainerLayout containerLayout = findViewById(R.id.search_tag);
+        List<String> tags = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            tags.add("标签" + i);
+        }
+        containerLayout.setTags(tags);
     }
 }
