ec3e327022f6ae64f6f635eebcea0245facac037
ThenWhat <2815383499@qq.com>
2020-01-07 11:02:30 +0800

    11:01

--- Project/Client/Oneworld/app/src/main/java/com/whut/oneworld/MainActivity.java
+++ Project/Client/Oneworld/app/src/main/java/com/whut/oneworld/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//dddfdsd
         // use banner viewpagger
         initImage();
         Banner banner = findViewById(R.id.ad_banner);
