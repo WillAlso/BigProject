7ce09f9de9e5ce4a094297d3004654cef68d2f85
1481660657@qq.com <tsrl5201314>
2020-01-08 20:30:54 +0800

    2020年1月8日20:07:25

--- Project/Client/Oneworld/app/src/main/java/com/whut/oneworld/exhibition/ExhibitionFragment.java
+++ Project/Client/Oneworld/app/src/main/java/com/whut/oneworld/exhibition/ExhibitionFragment.java
@@ -9,7 +9,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
